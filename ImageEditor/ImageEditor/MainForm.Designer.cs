c44c6ad5a6cffa8dfbab3fea48dd4307e41ef738
Sergey Shevchenko <shevchenko.serg.88@gmail.com>
2015-02-28 17:32:21 +0200

    commit #1

---
+++ ImageEditor/ImageEditor/MainForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace ImageEditor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.MainImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MainImage
+            // 
+            this.MainImage.BackColor = System.Drawing.Color.White;
+            this.MainImage.Location = new System.Drawing.Point(12, 35);
+            this.MainImage.Name = "MainImage";
+            this.MainImage.Size = new System.Drawing.Size(813, 420);
+            this.MainImage.TabIndex = 0;
+            this.MainImage.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(837, 467);
+            this.Controls.Add(this.MainImage);
+            this.Name = "MainForm";
+            this.Text = "ImageEditor";
+            ((System.ComponentModel.ISupportInitialize)(this.MainImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox MainImage;
+    }
+}
+
