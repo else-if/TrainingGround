fc7fd25a94a5fd259df96ce0daf8f69d58d77db4
Sergey Shevchenko <shevchenko.serg.88@gmail.com>
2015-03-01 22:43:20 +0200

    Commit #2

--- ImageEditor/ImageEditor/MainForm.Designer.cs
+++ ImageEditor/ImageEditor/MainForm.Designer.cs
@@ -29,34 +29,199 @@
         private void InitializeComponent()
         {
             this.MainImage = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbx1 = new System.Windows.Forms.TextBox();
+            this.tby1 = new System.Windows.Forms.TextBox();
+            this.tby2 = new System.Windows.Forms.TextBox();
+            this.tbx2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.MainImage)).BeginInit();
             this.SuspendLayout();
             // 
             // MainImage
             // 
             this.MainImage.BackColor = System.Drawing.Color.White;
-            this.MainImage.Location = new System.Drawing.Point(12, 35);
+            this.MainImage.Location = new System.Drawing.Point(12, 55);
             this.MainImage.Name = "MainImage";
-            this.MainImage.Size = new System.Drawing.Size(813, 420);
+            this.MainImage.Size = new System.Drawing.Size(813, 400);
             this.MainImage.TabIndex = 0;
             this.MainImage.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "x1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "y1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(116, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "x2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(116, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "y2";
+            // 
+            // tbx1
+            // 
+            this.tbx1.Location = new System.Drawing.Point(34, 6);
+            this.tbx1.Name = "tbx1";
+            this.tbx1.Size = new System.Drawing.Size(76, 20);
+            this.tbx1.TabIndex = 5;
+            // 
+            // tby1
+            // 
+            this.tby1.Location = new System.Drawing.Point(34, 29);
+            this.tby1.Name = "tby1";
+            this.tby1.Size = new System.Drawing.Size(76, 20);
+            this.tby1.TabIndex = 6;
+            // 
+            // tby2
+            // 
+            this.tby2.Location = new System.Drawing.Point(140, 29);
+            this.tby2.Name = "tby2";
+            this.tby2.Size = new System.Drawing.Size(76, 20);
+            this.tby2.TabIndex = 8;
+            // 
+            // tbx2
+            // 
+            this.tbx2.Location = new System.Drawing.Point(140, 6);
+            this.tbx2.Name = "tbx2";
+            this.tbx2.Size = new System.Drawing.Size(76, 20);
+            this.tbx2.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(337, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Draw LineTo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(236, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Fill test arrays";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(436, 9);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Draw MyLineTo";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(233, 39);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(433, 39);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "label6";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(580, 9);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(102, 23);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Simple MyLineTo";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(837, 467);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tby2);
+            this.Controls.Add(this.tbx2);
+            this.Controls.Add(this.tby1);
+            this.Controls.Add(this.tbx1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.MainImage);
             this.Name = "MainForm";
             this.Text = "ImageEditor";
             ((System.ComponentModel.ISupportInitialize)(this.MainImage)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox MainImage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbx1;
+        private System.Windows.Forms.TextBox tby1;
+        private System.Windows.Forms.TextBox tby2;
+        private System.Windows.Forms.TextBox tbx2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- ImageEditor/ImageEditor/MainForm.cs
+++ ImageEditor/ImageEditor/MainForm.cs
@@ -7,14 +7,272 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.Drawing.Imaging;
 
 namespace ImageEditor
 {
     public partial class MainForm : Form
     {
+        Point[] Aarray = new Point[10000];
+        Point[] Barray = new Point[10000];
+
+        const int pixelSize = 4; // 32 bits per pixel
+
         public MainForm()
         {
             InitializeComponent();
+            Bitmap b = new Bitmap(MainImage.Width, MainImage.Height);
+            MainImage.Image = (Image)b;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            Graphics gr = MainImage.CreateGraphics();
+            gr.Clear(Color.White);
+            Bitmap b = new Bitmap(MainImage.Width, MainImage.Height);
+            MainImage.Image = (Image)b;
+            MainImage.Refresh();
+
+            Stopwatch sw = new Stopwatch();
+
+            Bitmap bmp = (Bitmap)MainImage.Image;
+            Graphics g = Graphics.FromImage(bmp);
+            sw.Start();
+            for (int i = 0; i < Aarray.Length; i++)
+            {
+                LineTo(g, Aarray[i], Barray[i]);
+            }
+            sw.Stop();
+
+            MainImage.Refresh();
+
+            TimeSpan ts = sw.Elapsed;
+
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10);
+
+            label5.Text = elapsedTime;
+
+            /*
+            Point A = new Point();
+            Point B = new Point();
+
+            A.X = Convert.ToInt16(tbx1.Text);
+            A.Y = Convert.ToInt16(tby1.Text);
+            B.X = Convert.ToInt16(tbx2.Text);
+            B.Y = Convert.ToInt16(tby2.Text);
+            
+
+            MyLineTo(A, B);
+            MainImage.Refresh();
+
+            LineTo(new Point(A.X, A.Y + 25), new Point(B.X, B.Y + 25));
+             */
+        }
+
+        private void LineTo(Graphics g, Point A, Point B)
+        {
+            g.DrawLine(Pens.Black, A, B);
+        }
+
+        private void MyLineTo(Bitmap bmp, Point A, Point B)
+        {
+            //Bitmap bmp = (Bitmap)MainImage.Image;
+
+            int dx = Math.Abs(A.X - B.X);
+            int dy = Math.Abs(A.Y - B.Y);
+
+            int ind = 0;
+            int inc = 0;
+
+            Color col = Color.Black;
+
+            Point PLast;
+            Point PFirst;
+
+            //int stride = data.Stride;
+
+            BitmapData data = null;
+
+            unsafe
+            {
+                //byte* ptr = (byte*)data.Scan0;
+                int x, y;
+
+                if (dx >= dy)
+                {
+                    //by X ord
+                    float diff = dx / 2f;
+
+                    if (A.X >= B.X)
+                    {
+                        PLast = A;
+                        PFirst = B;
+                    }
+                    else
+                    {
+                        PLast = B;
+                        PFirst = A;
+                    }
+
+                    int k = (PLast.Y >= PFirst.Y) ? -1 : 1;
+
+                    do
+                    {
+                        if ((dy * ind) >= diff)
+                        {
+                            inc++;
+                            diff = diff + dx;
+                        }
+                        //bmp.SetPixel(PLast.X - ind, PLast.Y + k * inc, col);
+                        x = PLast.X - ind;
+                        y = PLast.Y + k * inc;
+
+                        data = bmp.LockBits(new Rectangle(x, y, 1, 1), ImageLockMode.WriteOnly, PixelFormat.Format1bppIndexed);
+                        bmp.UnlockBits(data);
+
+                        if (PLast.X - ind > PFirst.X + ind)
+                        //bmp.SetPixel(PFirst.X + ind, PFirst.Y - k * inc, col);
+                        {
+                            x = PFirst.X + ind;
+                            y = PFirst.Y - k * inc;
+
+                            data = bmp.LockBits(new Rectangle(x, y, 1, 1), ImageLockMode.WriteOnly, PixelFormat.Format1bppIndexed);
+                            bmp.UnlockBits(data);
+                        }
+
+                        ind++;
+
+                    } while (PLast.X - ind >= PFirst.X + ind);
+
+                }
+                else
+                {
+                    //by Y ord
+                    float diff = dy / 2f;
+
+                    if (A.Y >= B.Y)
+                    {
+                        PLast = A;
+                        PFirst = B;
+                    }
+                    else
+                    {
+                        PLast = B;
+                        PFirst = A;
+                    }
+
+                    int k = (PLast.X >= PFirst.X) ? -1 : 1;
+
+                    do
+                    {
+                        if ((dx * ind) >= diff)
+                        {
+                            inc++;
+                            diff = diff + dy;
+                        }
+                        //bmp.SetPixel(PLast.X + k * inc, PLast.Y - ind, col);
+                        x = PLast.X + k * inc;
+                        y = PLast.Y - ind;
+
+                        /*ptr[(x * 3) + y * stride] = 115;
+                        ptr[(x * 3) + y * stride + 1] = 115;
+                        ptr[(x * 3) + y * stride + 2] = 115;*/
+
+                        data = bmp.LockBits(new Rectangle(x, y, 1, 1), ImageLockMode.WriteOnly, PixelFormat.Format1bppIndexed);
+                        bmp.UnlockBits(data);
+
+                        if (PLast.Y - ind > PFirst.Y + ind)
+                        //bmp.SetPixel(PFirst.X - k * inc, PFirst.Y + ind, col);
+                        {
+                            x = PFirst.X - k * inc;
+                            y = PFirst.Y + ind;
+
+                            /*ptr[(x * 3) + y * stride] = 115;
+                            ptr[(x * 3) + y * stride + 1] = 115;
+                            ptr[(x * 3) + y * stride + 2] = 115;*/
+                            data = bmp.LockBits(new Rectangle(x, y, 1, 1), ImageLockMode.WriteOnly, PixelFormat.Format1bppIndexed);
+                            bmp.UnlockBits(data);
+                        }
+
+                        ind++;
+
+                    } while (PLast.Y - ind >= PFirst.Y + ind);
+                }
+            }
+
+            //bmp.UnlockBits(data);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int minX = 0;
+            int maxX = MainImage.Width - 1;
+            int minY = 0;
+            int maxY = MainImage.Height - 1;
+
+            Random r = new Random();
+            for (int i = 0; i < Aarray.Length; i++)
+            {
+                Aarray[i] = new Point(r.Next(minX, maxX), r.Next(minY, maxY));
+                Barray[i] = new Point(r.Next(minX, maxX), r.Next(minY, maxY));
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Graphics gr = MainImage.CreateGraphics();
+            gr.Clear(Color.White);
+            Bitmap b = new Bitmap(MainImage.Width, MainImage.Height);
+            MainImage.Image = (Image)b;
+            MainImage.Refresh();
+
+            Stopwatch sw = new Stopwatch();
+
+            //Graphics g = MainImage.CreateGraphics();
+            Bitmap bmp = (Bitmap)MainImage.Image;
+            
+            sw.Start();
+            for (int i = 0; i < Aarray.Length; i++)
+            {
+                MyLineTo(bmp, Aarray[i], Barray[i]);
+                //MainImage.Refresh();
+            }
+            sw.Stop();
+
+            MainImage.Refresh();
+
+            TimeSpan ts = sw.Elapsed;
+
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10);
+
+            label6.Text = elapsedTime;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Graphics gr = MainImage.CreateGraphics();
+            gr.Clear(Color.White);
+            Bitmap b = new Bitmap(MainImage.Width, MainImage.Height);
+            MainImage.Image = (Image)b;
+            MainImage.Refresh();
+            
+            Point A = new Point();
+            Point B = new Point();
+
+            A.X = Convert.ToInt16(tbx1.Text);
+            A.Y = Convert.ToInt16(tby1.Text);
+            B.X = Convert.ToInt16(tbx2.Text);
+            B.Y = Convert.ToInt16(tby2.Text);
+
+
+            MyLineTo((Bitmap)MainImage.Image, A, B);
+            MainImage.Refresh();
         }
     }
 }
