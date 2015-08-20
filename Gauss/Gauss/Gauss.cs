ade8aee6d12039583b5816db9c672a91aa66f745
Sergey Shevchenko <shevchenko.serg.88@gmail.com>
2015-08-21 00:06:46 +0300

    21.03

---
+++ Gauss/Gauss/Gauss.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GaussSolution
+{
+    class Gauss
+    {
+        public List<GaussStep> Steps { get; private set; }
+        private GaussStep FirstStep; 
+        private double[] X;
+        private double[] E;
+        private int varCount;
+        public string ErrMsg { get; private set; }
+
+        public Gauss(int variablesCount, double[,] A, double[] B)
+        {
+            varCount = variablesCount;
+            X = new double[varCount];
+            E = new double[varCount];
+
+            FirstStep = new GaussStep(varCount, A, B);
+            ErrMsg = "";
+        }
+
+        public void Calculate()
+        {
+            Steps = new List<GaussStep>();
+            Steps.Add(FirstStep);
+
+            double [,]A = new double[varCount, varCount];
+            double[] B = new double[varCount];
+
+            Array.Copy(FirstStep.A, A, FirstStep.A.Length);
+            Array.Copy(FirstStep.B, B, FirstStep.B.Length);
+
+            //прямой ход
+            for (int i = 0; i < varCount - 1; i++)
+            {
+                //возможно нужно переставить местами строки
+                if (A[i, i] == 0)
+                {
+                    for (int j = i + 1; j < varCount - 1; j++)
+                    {
+                        if (A[j, i] != 0)
+                        {
+                            //переставляем строки
+                            for (int k = i; k < varCount - 1; k++)
+                            {
+                                double temp = A[i, k];
+                                A[i, k] = A[j, k];
+                                A[j, k] = temp;
+                            }
+
+                            break;
+                        }
+                    }
+
+                    if (A[i, i] == 0)
+                    {
+                        //методом Гаусса решить невозможно
+                        ErrMsg = "методом Гаусса решить невозможно";
+                        break;
+                    }
+                }
+
+                double basicValue = A[i,i];
+                for (int j = i; j < varCount - 1; j++)
+                {
+                    A[i, j] = A[i, j] / basicValue;
+                }
+                B[i] = B[i] / basicValue;
+
+
+                for (int j = i + 1; j < varCount - 1; j++)
+                {
+                    basicValue = A[j, i];
+                    for (int k = i; k < varCount - 1; k++)
+                    {
+                        A[j, k] = A[j, k] - A[i, k] * basicValue;
+                    }
+                    B[j] = B[j] - B[i] / basicValue;
+                }
+
+                Steps.Add(new GaussStep(varCount, A, B));
+            }
+
+        }
+    }
+}
---
+++ Gauss/Gauss/GaussStep.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GaussSolution
+{
+    class GaussStep
+    {
+        public double[,] A { get; private set; }
+        public double[] B { get; private set; }
+
+        public GaussStep(int variablesCount, double[,] AMatrix, double[] BMatrix)
+        {
+            A = new double[variablesCount, variablesCount];
+            B = new double[variablesCount];
+
+            Array.Copy(AMatrix, A, AMatrix.Length);
+            Array.Copy(BMatrix, B, BMatrix.Length);
+        }
+    }
+}
